36f9ac96ee9094348be055bcc575c3d32cb62862
Charity Marani <charitymarani@gmail.com>
2018-08-13 05:41:03 +0300

    Remove old UI files

--- UI/javascript/onclik.js
+++
@@ -1,5 +0,0 @@
-<script>
-function openWin() {
-    window.location.href=("question_answer.html");
-}
-</script>
--- UI/javascript/profile.js
+++
@@ -1,4 +0,0 @@
-function editProfile(){
-		document.getElementById("content").style.display="none";
-		document.getElementById("edit").style.display="block";
-	}
