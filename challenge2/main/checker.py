8c889faa28686b9a4131970ca7d075b5cff6f387
Charity Marani <charitymarani@gmail.com>
2018-08-19 13:27:01 +0300

    update api endpoints

---
+++ challenge2/main/checker.py
@@ -0,0 +1,20 @@
+'''checker.py'''
+from challenge2.models import ALL_QUESTIONS
+
+
+def question_already_exist(id):
+    ''' check if an object exist'''
+    if id in ALL_QUESTIONS:
+        print('Question already exists')
+    return False
+
+
+def answer_already_exist(list_, object_key, object_attr):
+    ''' find out if an object exist'''
+    object_list = list(
+        filter(
+            lambda object_dict: object_dict[object_key] == object_attr,
+            list_))
+    if object_list:
+        return object_list
+    return False
